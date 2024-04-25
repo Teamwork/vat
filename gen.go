75b5ab4603df044cad3ccbc91fd5845019ba5d70
Chris Rollins <3865054+chrisrollins65@users.noreply.github.com>
2024-04-25 09:28:43 +0200

    I guess remove this blank import as it seems to be unnecessary

--- gen.go
+++ gen.go
@@ -1,5 +1,3 @@
 //go:generate mockgen -destination=mocks/mock_lookup_service.go -package=mocks github.com/teamwork/vat/v3 LookupServiceInterface
 
 package vat
-
-import _ "github.com/golang/mock/mockgen/model" // Blank import to ensure the model is included in the binary.
